2bf86faaadb2803debde35ff9266b75e8467eea3
udayasherkar94-prog <udayasherkar94@gmail.com>
2026-01-16 18:15:06 +0530

    day_2 task  completed

---
+++ day_2.js
@@ -0,0 +1,69 @@
+
+//task 1)
+console.log("day 2 Variables Abd dataTypes In JS");
+
+// var name ="ajit";
+// var age = 21;
+// var status = "student";
+// var fav_proLang = 'js'
+
+// console.log("\tname:"+name +"\tage:"+age+"\tstatus:"+status+"\tfaviourite programming lang:"+fav_proLang);
+
+//task 2)
+let name ="ajit";
+let age = 21;
+let status = "student";
+let fav_proLang = 'js'
+
+
+ name ="suresh";
+ age = 25;
+ status = "working ";
+ fav_proLang = 'c++'
+
+//it updates the value which is resent assign to the same variable
+console.log("\tname:"+name +"\tage:"+age+"\tstatus:"+status+"\tfaviourite programming lang:"+fav_proLang);
+
+
+//task 3)
+// const name ="ajit";
+// const age = 21;
+// const status = "student";
+// const fav_proLang = 'js'
+
+
+//  name ="suresh";
+//  age = 25;
+//  status = "working ";
+//  fav_proLang = 'c++'
+
+
+// console.log("\tname:"+name +"\tage:"+age+"\tstatus:"+status+"\tfaviourite programming lang:"+fav_proLang);
+//error assignment to constant variabke
+
+
+
+//task4)
+
+//Object
+let person = {
+  name: "Ajit",
+  age: 21,
+  status: "student",
+  skills: "JavaScript"
+};
+
+console.log(person);
+console.log(person.name);   // Ajit
+console.log(person.age);    // 21
+console.log(typeof(person))
+
+//Array
+let languages = ["JavaScript", "HTML", "CSS", "React"];
+
+console.log(languages);
+console.log(languages[0]); // JavaScript
+console.log(languages[2]); // CSS
+languages.push("Node.js");
+console.log(typeof(person));
+
